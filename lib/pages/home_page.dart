c00850adb341f189a6af850ef8d30a2d4c13ff0a
rizkijuliansyaham <rizki.juliansyah.a.m@gmail.com>
2022-07-07 12:10:05 +0700

    fix starter

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/coinbase_bottom_bar.dart';
+import '../widgets/top_bar.dart';
+import '../widgets/asset_view_header.dart';
+import '../widgets/balance_header.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: ListView(
+            children: [
+              TopBar(),
+              BalanceHeader(balance: 259.54),
+              AssetsViewHeader(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: CoinbaseBottomBar(
+        selectedIndex: (i) {
+          // change the page according to the index
+          // uncomment in case you want to implement more pages and make sure
+          // to create a _selectedIndex variable
+
+          // setState((){
+          //   _selectedIndex = i;
+          // })
+        },
+      ),
+    );
+  }
+}
