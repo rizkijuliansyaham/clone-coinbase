1ef1119b46b09a64102678a9127bef936c0a6c9d
rizkijuliansyaham <rizki.juliansyah.a.m@gmail.com>
2022-07-12 22:56:52 +0700

    menambahkan tampilan detail dan tombol Trade

--- lib/pages/coin_page.dart
+++ lib/pages/coin_page.dart
@@ -1,3 +1,4 @@
+import 'package:belajar_clone_coinbase/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -60,6 +61,72 @@ class _CoinPageState extends State<CoinPage> {
             ),
 
             // Nanti grafik disini
+
+            //
+            //
+            //
+
+            Text(
+              widget.coin.fullName,
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              widget.coin.ticker,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 1,
+              color: Colors.grey.shade300,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+
+            Text(
+              "Algorithm: ${widget.coin.algorithm}",
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+            ),
+            Text(
+              "Block number: ${widget.coin.blockNumber.toStringAsFixed(2)}",
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+            ),
+            Text(
+              "Block reward: ${widget.coin.blockReward.toStringAsFixed(2)}",
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+            ),
+            Text(
+              "Block time: ${widget.coin.blockTime.toStringAsFixed(2)}",
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+            ),
+            Text(
+              "Max supply: ${widget.coin.maxSupply}",
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+
+            //
+            //
+
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                "Trade",
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25))),
+                  padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(horizontal: 0, vertical: 15)),
+                  backgroundColor:
+                      MaterialStateProperty.all(CoinbaseTheme.color)),
+            )
           ],
         ),
       )),
